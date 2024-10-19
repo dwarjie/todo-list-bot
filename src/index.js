30435b8c35622d7794166fd68bf42d3a6a93b896
dwarjie <macoycoder@gmail.com>
2024-10-19 18:19:44 +0800

    Fixed the directory structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const fs = require("node:fs");
 const path = require("node:path");
-const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
 
 const client = new Client({
 	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
-client.commands = new Collection();
 
+client.commands = new Collection();
 const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
@@ -30,40 +30,19 @@ for (const folder of commandFolders) {
 	}
 }
 
-client.on(Events.InteractionCreate, async (interaction) => {
-	if (!interaction.isChatInputCommand()) return;
+const eventPath = path.join(__dirname, "events");
+const eventFiles = fs
+	.readdirSync(eventPath)
+	.filter((file) => file.endsWith(".js"));
 
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
+for (const file of eventFiles) {
+	const filePath = path.join(eventPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-
-	try {
-		await command.execute(interaction);
-	} catch (err) {
-		console.error(err);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({
-				content: `There was an error while executing this command!`,
-				ephemeral: true,
-			});
-		} else {
-			await interaction.reply({
-				content: `There was an error while executing this command!`,
-				ephemeral: true,
-			});
-		}
-	}
-});
-
-client.once(Events.ClientReady, (readyClient) => {
-	console.log(`Logged in as $${readyClient.user.tag}`);
-	readyClient.user.setPresence({
-		activities: [{ name: "💻 with discord.js" }],
-		status: "dnd",
-	});
-});
+}
 
 client.login(process.env.TOKEN);
